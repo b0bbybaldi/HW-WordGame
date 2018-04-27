2b2ff8d0539526613a15aa8a3ebf854564d51ae6
b0bbybaldi <roberto.baldizon@silibrain.com>
2018-04-26 23:38:34 -0700

    Not fullt functioning game submitted, will continue working on it

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,52 +1,57 @@
+var artists = ["bad bunny", "daddy yankee", "don omar", "enrique iglesias", "farruko", "j balvin", "juan magan", "luis fonsi", "maluma", "nicky jam", "ozuna", "piso 21", "sebastian yatra", "wisin", "yandel"];
+
+var randomArtist = artists[(Math.floor(Math.random()*artists.length))];
+
+console.log(randomArtist);
+
 var lives = 10;
 
 var spaces = [];
 
 var wrongs = [];
 
-var tries = document.getElementById("guess")
+var wins = document.getElementById("score");
+
+var tries = document.getElementById("guess");
 
 var userText = document.getElementById("name");
 
-var wins = document.getElementById("score");
-
-var artists = ["BAD BUNNY", "DADDY YANKEE", "DON OMAR", "ENRIQUE IGLESIAS", "FARRUKO", "J BALVIN", "JUAN MAGAN", "LUIS FONSI", "MALUMA", "NICKY JAM", "OZUNA", "PISO 21", "SEBASTIAN YATRA", "WISIN", "YANDEL"];
-  
-var randomArtist = artists[(Math.floor(Math.random()*artists.length))]
-
-console.log(randomArtist);
-
-var underscore = (" _ ").repeat(randomArtist.length)
-
+// var getSpaces = function(){
+  for(var i=0; i<randomArtist.length;i++){
+    spaces.push("_");
+  }
+//   return spaces;
+// }
 
 
 document.onkeyup = function(event){
+
   var userGuess = event.keyCode;
 
   console.log(userGuess);
 
-  var word = String.fromCharCode(userGuess)
+  var word = String.fromCharCode(userGuess);
 
-  console.log(word)
+  console.log(word);
 
   if(randomArtist.indexOf(word)>-1){
     spaces[randomArtist.indexOf(word)] = word;
-    userText[0].innerHTML = spaces.join(" ");
+    userText.textContent = spaces.join(" ");
 
-    if(userText === randomArtist){
+    if(spaces.join(" ") == randomArtist){
       alert("You win!");
-      wins++
+      wins++;
     } 
   }else{
+      lives--
     if(lives === 0){
       alert(" You lost! -> Game Over :( ");
     }else{
-    wrongs.push(word);
+      wrongs.push(word);
+      tries.textContent = wrongs;
     }
   }
 
 }
-
-userText.textContent = underscore;
 
    
