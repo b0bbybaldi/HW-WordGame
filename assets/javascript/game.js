f451ab4637b1c971676ffe9380fad5b8d44af725
b0bbybaldi <roberto.baldizon@silibrain.com>
2018-04-27 02:07:57 -0700

    game is not displaying all the letters nor checking for them, however it seems to be working well for the first letter

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,18 @@
 var choices =['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
-var artists = ["bad bunny", "daddy yankee", "don omar", "enrique iglesias", "farruko", "j balvin", "juan magan", "luis fonsi", "maluma", "nicky jam", "ozuna", "piso 21", "sebastian yatra", "wisin", "yandel"];
+var artists = ['bad bunny', 'daddy yankee', 'don omar', 'enrique iglesias', 'farruko', 'j balvin', 'juan magan', 'luis fonsi', 'maluma', 'nicky jam', 'ozuna', 'piso 21', 'sebastian yatra', 'wisin', 'yandel'];
 
 var randomArtist = "";
 
 var letters = [];
 
-var success = [];
+var hits = [];
 
 var wrongs = [];
 
 var wins = 0;
 
-var lives = 10;
+var tries = 10;
 
 var spaces = 0;
 
@@ -21,68 +21,90 @@ var rights = 0;
 function reset(){
 
   randomArtist = artists[(Math.floor(Math.random()*artists.length))];
-
+ 
   letters = randomArtist.split('');
 
   spaces = letters.length;
 
-  console.log(randomArtist);
-
-  lives = 10;
+  tries = 10;
 
   rights = 0;
 
-  success = [];
+  hits = [];
 
   wrongs = [];
 
   choices =['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
+  test = false;
 
-  for(var i=0; i<letters.length;i++){
-    success.push("_");
-    document.getElementById("name").innerHTML = success;
-  }
-  
-  document.getElementById("name").innerHTML = success.join('');
-
-  document.getElementById("lives").innerHTML = lives;
-
-  document.getElementById("score").innerHTML = wins;
-
+  start();
 }
 
+function start(){
+  randomArtist = artists[(Math.floor(Math.random()*artists.length))];
+  console.log(randomArtist);
+
+  letters = randomArtist.split('');
+
+  spaces = letters.length;
+
+  tries = 10;
+
+  rights = 0;
+
+  hits = [];
+
+  wrongs = [];
+
+  choices =['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
+  for(var i=0; i<spaces;i++){
+    hits.push(' _ ');
+    document.getElementById('name').innerHTML = hits;
+  }
+
+  document.getElementById('name').innerHTML = hits.join(' ');
+
+  document.getElementById('lives').innerHTML = tries;
+
+  document.getElementById('score').innerHTML = wins;
+}
+
+start();
 
 document.onkeyup = function(event){
+
+  test = true;
 
   var userGuess = event.key;
 
   console.log(userGuess);
 
   for(var i=0; i<choices.length; i++){
-    if(userGuess === choices[i]){
+    if(userGuess === choices[i] && test === true){
       if(randomArtist.indexOf(userGuess) > -1){
         for(var i=0; i< spaces; i++){
           if(letters[i] === userGuess){
             rights++;
-            success[i] = userGuess;
-            document.getElementById("name").innerHTML = success.join(" ");
+            hits[i] = userGuess;
+            document.getElementById('name').innerHTML = hits.join(' ');
           }
         }
       }else{
         wrongs.push(userGuess);
-        lives--;
-        document.getElementById("lives").innerHTML = lives;
-        document.getElementById("guess").innerHTML = wrongs;
+        tries--;
+        document.getElementById('lives').innerHTML = tries;
+        document.getElementById('guess').innerHTML = wrongs;
       }
 
       if(rights === spaces){
         wins++;
-        document.getElementById("score").innerHTML = wins;
-        alert("You Win");
+        document.getElementById('score').innerHTML = wins;
+        alert('You Won!');
         reset();
-      }else if(lives === 0){
-        alert("You Loose");
+      }else if(tries === 0){
+        alert('You Lost :(');
         reset();
       }
     }
